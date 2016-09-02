d1f3a5a9c239fd36db26309b6c0883fd3d910ed5
Haggai Yaniv <haggai@getjaco.com>
2016-09-02 13:56:27 +0300

    10_display_photos

---
+++ client/components/comment.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Comment extends React.Component {
+  render() {
+    return <div>Comment</div>;
+  }
+}
+
+export default Comment;
--- client/components/single.js
+++ client/components/single.js
@@ -1,8 +1,17 @@
 import React from 'react';
-
+import Photo from './photo';
+import Comment from './comment';
 class Single extends React.Component {
     render() {
-      return <div className="single-photo">I'm the single mark</div>
+      // get the index
+      const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+      const post = this.props.posts[i];
+      return (
+        <div className="single-photo">
+          <Photo index={i} post={post} {...this.props} />
+          <Comment></Comment>
+        </div>
+      )
     }
 }
 
